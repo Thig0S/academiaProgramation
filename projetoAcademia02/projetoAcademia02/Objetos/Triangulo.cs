ed8e9cff758c8215317a7b81c9eff58604d3ba68
Thig0S <thiagokovalski5@gmail.com>
2026-02-26 22:45:00 -0300

    Projeto 2

---
+++ projetoAcademia02/projetoAcademia02/Objetos/Triangulo.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projetoAcademia02.Objetos
+{
+    internal class Triangulo
+    {
+         
+        private double X { get; set; }
+        private double Y { get; set; }
+        private double Z { get; set; }
+
+
+        public void AplicarVariaveis(double x, double y, double z)
+        {
+
+            this.X = x;
+            this.Y = y;
+            this.Z = z;
+
+        }
+
+        public void DefinirTriangulo() //Verifica qual triangulo é
+        {
+
+            if (this.X == this.Y && this.X == this.Z)
+            {
+                Console.WriteLine("\nTriangulo é Equilatero");
+            }
+            else if (this.X == this.Y || this.X == this.Y || this.Y == this.Z)
+            {
+                Console.WriteLine("\nTriangulo é Isosceles");
+            }
+            else
+            {
+                Console.WriteLine("\nTriangulo é Escaleno");
+            }
+        }
+        public void Enter() // só para resetar o console e n ficar feio
+        {
+            string enter;
+            Console.WriteLine("Pressione ENTRE para continuar...");
+            enter = Console.ReadLine();
+            Console.Clear();
+        }
+    }
+}
---
+++ projetoAcademia02/projetoAcademia02/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using projetoAcademia02.Objetos;
+
+namespace projetoAcademia02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+
+                Triangulo triangulo = new Triangulo();
+
+                Console.WriteLine("Olá bem-vindo ao verificador de triangulos!\n");
+                Console.WriteLine("Escolha uma opção:\n 1 - Verificar triangulo\n 2 - Sair do Programa\n");
+
+                double escolha;
+
+                while (true) // verifica se a escolha e valida
+                {
+                    try
+                    {
+                        escolha = double.Parse(Console.ReadLine());
+                        if (escolha == 1 || escolha == 2)
+                        {
+                            break;
+                        }
+                        else
+                        {
+                            throw new ArgumentException("Digite apenas 1 ou 2:\n");
+                        }
+                    }
+                    catch (Exception)
+                    {
+                        Console.Write("\nDigite 1 ou 2!\n");
+                    }
+                }
+                if (escolha == 2) //encerra o programa
+                {
+                    Console.WriteLine("\nObrigado meu fera, até mais!");
+                    break;
+                }
+                Console.Clear();
+
+                while (true) //verificar se o triangulo e valido
+                {
+                    try
+                    {
+                        Console.WriteLine("\nDigite o valor do lado X: ");
+                        double x = double.Parse(Console.ReadLine());
+
+                        Console.WriteLine("\nDigite o valor do lado Y");
+                        double y = double.Parse(Console.ReadLine());
+
+                        Console.WriteLine("\nDigite o valor do lado Z");
+                        double z = double.Parse(Console.ReadLine());
+
+                        if (z < 0 || y < 0 || x < 0) //verifica se todos os numeros sao positivos
+                        {
+                            throw new ArgumentException("Digite apenas valores POSITIVOS");
+                        }
+
+                        if (x + y > z && y + z > x && z + x > z) //verifica se o triangulo existe msm
+                        {
+                            //triangulo valido
+                            triangulo.AplicarVariaveis(x, y, z);
+                            break;
+                        }
+                        else
+                        {
+                            throw new ArgumentException("Digite um triangulo Valido");
+
+                        }
+                    }
+                    catch
+                    {
+                        Console.WriteLine("Digite apenas números POSITIVOS e VALIDOS");
+                        triangulo.Enter();
+                    }
+                }
+                triangulo.DefinirTriangulo();
+                triangulo.Enter();
+            }
+        }
+    } 
+}
